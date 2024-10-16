93ba32012d9f0482516b9af51f6ed82523b0fad7
pmsmirnov <pmsmirnov@mail.ru>
2024-10-16 15:31:13 +0300

    first

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Comparing singleton beans: " + (bean1 == bean));
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        System.out.println("Comparing prototype beans: " + (cat1 == cat));
+
     }
 }
